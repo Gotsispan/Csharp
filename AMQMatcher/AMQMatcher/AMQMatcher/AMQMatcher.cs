24edccb5d9a58c76ad56bd5a3ace251c5af41c4d
Panagiotis Gotsis <89860561+Gotsispan@users.noreply.github.com>
2023-04-11 09:48:27 +0300

    Add files via upload

--- AMQMatcher/AMQMatcher/AMQMatcher/AMQMatcher.cs
+++ AMQMatcher/AMQMatcher/AMQMatcher/AMQMatcher.cs
@@ -9,31 +9,18 @@ using System.Globalization;
 using static System.Net.WebRequestMethods;
 using System.Collections;
 using System.Security.Cryptography;
-using System.ComponentModel.DataAnnotations.Schema;
 
 namespace AMQMatcher
 
 {
     class Program
     {
-        static void findallchoices () {
-            string fileContent = new WebClient().DownloadString("https://raw.githubusercontent.com/Gotsispan/Csharp/main/Eurovision%20Simulator/Eurovision%20Simulator/votes.csv");
-            string[] words = fileContent.Split("\n");
-            var words2 = new string[words.Length, 3];
-        }
-        static void findmissing () {
-            string fileContent = new WebClient().DownloadString("https://raw.githubusercontent.com/Gotsispan/Csharp/main/Eurovision%20Simulator/Eurovision%20Simulator/votes.csv");
-            string[] words = fileContent.Split("\n");
-            var words2 = new string[words.Length, 3];
-        }
-
-
         static void Main(string[] args)
         {
             Console.OutputEncoding = Encoding.UTF8;
             Console.OutputEncoding = Encoding.Unicode;
 
-            string appname = "AMQ Song/Artist matcher";
+            string appname = "Eurovision Simulator";
             string appversion = "1.0.0";
             string appauthor = "Gotsispan";
             string authortext = appname + " : version " + appversion + " by " + appauthor;
