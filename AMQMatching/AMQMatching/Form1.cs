9ef403c5cbd6bed2ee5f6570bc914a556c102e1e
Panagiotis Gotsis <89860561+Gotsispan@users.noreply.github.com>
2023-04-12 09:29:52 +0300

    Add files via upload

---
+++ AMQMatching/AMQMatching/Form1.cs
@@ -0,0 +1,84 @@
+﻿using AMQMatching;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace AMQMatcher
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.Load += new System.EventHandler(this.Form1_Load);
+        }
+        public string[] artistsarray { get; set; }
+        public string[] songsarray { get; set; }
+        public string[] animearray { get; set; }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+            textBox1.AutoCompleteMode = AutoCompleteMode.Suggest;
+            textBox1.AutoCompleteSource = AutoCompleteSource.CustomSource;
+            AutoCompleteStringCollection autoCompleteCollection1 = new AutoCompleteStringCollection();
+            autoCompleteCollection1.AddRange(artistsarray);
+            textBox1.AutoCompleteCustomSource = autoCompleteCollection1;
+
+            textBox2.AutoCompleteMode = AutoCompleteMode.Suggest;
+            textBox2.AutoCompleteSource = AutoCompleteSource.CustomSource;
+            AutoCompleteStringCollection autoCompleteCollection2 = new AutoCompleteStringCollection();
+            autoCompleteCollection2.AddRange(songsarray);
+            textBox2.AutoCompleteCustomSource = autoCompleteCollection2;
+
+            textBox3.AutoCompleteMode = AutoCompleteMode.Suggest;
+            textBox3.AutoCompleteSource = AutoCompleteSource.CustomSource;
+            AutoCompleteStringCollection autoCompleteCollection3 = new AutoCompleteStringCollection();
+            autoCompleteCollection3.AddRange(animearray);
+            textBox3.AutoCompleteCustomSource = autoCompleteCollection3;
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(textBox1.Text);
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = DateTime.Now.ToString();
+        }
+
+        private void textBox1_TextChanged_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Mylablel_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+}
---
+++ AMQMatching/AMQMatching/Program.cs
@@ -0,0 +1,111 @@
+﻿using AMQMatcher;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.VisualStyles;
+
+namespace AMQMatching
+{
+     public class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        static void arraytype(string[] arr)
+        {
+            string txt = "[";
+            foreach (var item in arr)
+            {
+                txt = txt + item;
+                txt = txt + ",";
+            }
+            txt = txt.Remove(txt.Length - 1);
+            txt = txt + "] \n";
+            Console.WriteLine(txt);
+        }
+
+        static List<string[]> readdatabase()
+        {
+            string fileContent = new WebClient().DownloadString("https://raw.githubusercontent.com/Gotsispan/Csharp/main/AMQMatcher/AMQSongsDatabase.txt");
+            string[] words = fileContent.Split('\n');
+            var words2 = new List<string[]>();
+            for (int i = 0; i < words.Length; i++)
+            {
+                words2.Add(words[i].Split('|'));
+            }
+            return words2;
+        }
+
+
+        static void printlist(List<string[]> list)
+        {
+
+            for (int i = 0; i < list.Count; i++)
+            {
+                for (int j = 0; j < list[i].Length; j++)
+                {
+                    Console.Write(list[i][j]);
+                    Console.Write(' ');
+                }
+                Console.Write("\n");
+            }
+        }
+        static string[] addtoarraystr(string[] array, string ele)
+        {
+            Array.Resize(ref array, array.Length + 1);
+            array[array.Length - 1] = ele;
+            return array;
+        }
+
+
+
+        [STAThread]
+
+
+
+
+
+        public static void Main(string[] args)
+        {
+            var words2 = readdatabase();
+            Dictionary<int, string> indextoartist = new Dictionary<int, string>();
+            Dictionary<int, string> indextosong = new Dictionary<int, string>();
+            Dictionary<int, string> indextoanime = new Dictionary<int, string>();
+            string[] artistsall = { };
+            string[] songsall = { };
+            string[] animeall = { };
+
+            for (int i = 0; i < words2.Count - 1; i++)
+            {
+                
+                if (!artistsall.Contains(words2[i][1]))
+                {
+                    artistsall = addtoarraystr(artistsall, words2[i][1]);
+                }
+                if (!songsall.Contains(words2[i][0]))
+                {
+                    songsall = addtoarraystr(songsall, words2[i][0]);
+                }
+                if (!animeall.Contains(words2[i][2]))
+                {
+                    animeall = addtoarraystr(animeall, words2[i][2]);
+                }
+                indextoartist.Add(key: i, value: words2[i][0]);
+                indextosong.Add(key: i, value: words2[i][1]);
+                indextoanime.Add(key: i, value: words2[i][2]);
+            }
+
+            Form1 form = new Form1();
+            form.artistsarray = artistsall;
+            form.songsarray = songsall;
+            form.animearray = animeall;
+            form.Show();
+            Application.EnableVisualStyles();
+            Application.Run(form);
+        }
+    }
+}
